da3f085ff86ecd828e840d7967a5492944602093
Panji Yoga <panjiyogap@gmail.com>
2023-03-10 15:24:57 +0700

    create project pinflix

---
+++ pinflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pinflix
+//
+//  Created by Panji Yoga on 10/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
