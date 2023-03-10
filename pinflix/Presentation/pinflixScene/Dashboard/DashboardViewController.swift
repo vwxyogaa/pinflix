69570e0eb6dd7f35e622bf06f7c45e23f9005f1b
Panji Yoga <panjiyogap@gmail.com>
2023-03-10 17:39:10 +0700

    dashboard : config search

--- pinflix/Presentation/pinflixScene/Dashboard/DashboardViewController.swift
+++ pinflix/Presentation/pinflixScene/Dashboard/DashboardViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
+    @IBOutlet weak var searchMovieTextField: UITextField!
     
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViews()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -25,5 +28,18 @@ class DashboardViewController: UIViewController {
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+    
+    // MARK: - Helpers
+    private func configureViews() {
+        configureSearchTextField()
+    }
+    
+    private func configureSearchTextField() {
+        guard let grayTabBarColor = UIColor(named: "GrayTabBarColor") else { return }
+        searchMovieTextField.attributedPlaceholder = NSAttributedString(
+            string: "Search",
+            attributes: [NSAttributedString.Key.foregroundColor: grayTabBarColor]
+        )
     }
 }
