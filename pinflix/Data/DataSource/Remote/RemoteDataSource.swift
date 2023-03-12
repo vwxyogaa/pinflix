cf133fee6c4409a66d1a748623a7d77a7fb764c4
yxgg <yogajktf4@gmail.com>
2023-03-12 17:51:06 +0700

    remote data source : getnowplaying

---
+++ pinflix/Data/DataSource/Remote/RemoteDataSource.swift
@@ -0,0 +1,20 @@
+//
+//  RemoteDataSource.swift
+//  pinflix
+//
+//  Created by yxgg on 12/03/23.
+//
+
+import Foundation
+import RxSwift
+
+final class RemoteDataSource {
+    private let baseUrl = "https://api.themoviedb.org/3/movie"
+    private let apiKey = "3ff97919634cab922c295f2d9b5ff64a"
+    
+    func getNowPlaying() -> Observable<[TMDB.Results]> {
+        let url = URL(string: baseUrl + "/now_playing?api_key=\(apiKey)")!
+        let data: Observable<[TMDB.Results]> = APIManager.shared.executeQuery(url: url, method: .get, params: nil)
+        return data
+    }
+}
