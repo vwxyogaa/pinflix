7fe7edcef4293bbe7374b0367d6e042c5d5f3f5e
yxgg <yogajktf4@gmail.com>
2023-03-12 17:51:41 +0700

    repository : getnowplaying

---
+++ pinflix/Data/Repository/Repository.swift
@@ -0,0 +1,34 @@
+//
+//  Repository.swift
+//  pinflix
+//
+//  Created by yxgg on 12/03/23.
+//
+
+import Foundation
+import RxSwift
+
+protocol RepositoryProtocol {
+    // MARK: - remote
+    func getNowPlaying() -> Observable<[TMDB.Results]>
+}
+
+final class Repository: NSObject {
+    typealias MovieInstance = (RemoteDataSource) -> Repository
+    fileprivate let remote: RemoteDataSource
+    
+    init(remote: RemoteDataSource) {
+        self.remote = remote
+    }
+    
+    static let sharedInstance: MovieInstance = { remote in
+        return Repository(remote: remote)
+    }
+}
+
+extension Repository: RepositoryProtocol {
+    // MARK: - remote
+    func getNowPlaying() -> Observable<[TMDB.Results]> {
+        return remote.getNowPlaying()
+    }
+}
