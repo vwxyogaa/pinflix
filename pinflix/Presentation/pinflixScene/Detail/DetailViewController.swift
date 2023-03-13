a7287d3637978b235ab0f46475a049d3159a57ed
Panji Yoga <panjiyogap@gmail.com>
2023-03-13 16:12:14 +0700

    create detail view controller

---
+++ pinflix/Presentation/pinflixScene/Detail/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  pinflix
+//
+//  Created by Panji Yoga on 13/03/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
