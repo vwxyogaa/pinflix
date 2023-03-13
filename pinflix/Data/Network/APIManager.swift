73ec8cc901f049806202c1e02ef20341640d0bfd
Panji Yoga <panjiyogap@gmail.com>
2023-03-13 13:44:08 +0700

    change variable name token

--- pinflix/Data/Network/APIManager.swift
+++ pinflix/Data/Network/APIManager.swift
@@ -11,12 +11,12 @@ import RxSwift
 
 final class APIManager {
     static let shared = APIManager()
-    private let apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZmY5NzkxOTYzNGNhYjkyMmMyOTVmMmQ5YjVmZjY0YSIsInN1YiI6IjY0MDk4NTVjZjlhM2ZiMDA3YWZhOTJmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yzIluEWS7mkD_UtYhRvhd6ZSGYAz-eKa8hAQrxTavPk"
+    private let apiReadAccessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZmY5NzkxOTYzNGNhYjkyMmMyOTVmMmQ5YjVmZjY0YSIsInN1YiI6IjY0MDk4NTVjZjlhM2ZiMDA3YWZhOTJmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yzIluEWS7mkD_UtYhRvhd6ZSGYAz-eKa8hAQrxTavPk"
     
     func executeQuery<T> (url: URL, method: HTTPMethod, params: Parameters? = nil) -> Observable<T> where T: Decodable {
         return Observable<T>.create { observer in
             let headers: HTTPHeaders = [
-                "Authorization": "Bearer \(self.apiKey)",
+                "Authorization": "Bearer \(self.apiReadAccessToken)",
                 "Content-Type": "application/json;charset=utf-8"
             ]
             AF.request(url, method: method, parameters: params, headers: headers).validate().responseDecodable(of: T.self) { response in
