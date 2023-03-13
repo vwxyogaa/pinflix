cc9bfef54244e5eec01ddb39e1f12a6f3c5c4cea
Panji Yoga <panjiyogap@gmail.com>
2023-03-13 13:39:55 +0700

    refactor entities tmdb

--- pinflix/Domain/Entities/TMDB.swift
+++ pinflix/Domain/Entities/TMDB.swift
@@ -9,55 +9,47 @@ import Foundation
 
 struct TMDB: Codable {
     let dates: Dates
-    let page: Int
     let results: [Results]
-    let totalPages: Int
-    let totalResults: Int
-    
+    let totalPages, totalResults, page: Int
+
     enum CodingKeys: String, CodingKey {
-        case dates
-        case page
-        case results
+        case dates, results
         case totalPages = "total_pages"
         case totalResults = "total_results"
+        case page
     }
     
     struct Dates: Codable {
-        let maximum: String
-        let minimum: String
+        let maximum, minimum: String
     }
     
     struct Results: Codable {
+        let genreIDS: [Int]
         let adult: Bool
         let backdropPath: String
-        let genreIds: [Int]
         let id: Int
-        let originalLanguage: String
         let originalTitle: String
-        let overview: String
-        let popularity: Double
-        let posterPath: String
-        let releaseDate: String
-        let title: String
-        let video: Bool
-        let voteAverage: Double
+        let voteAverage, popularity: Double
+        let posterPath, overview, title: String
+        let originalLanguage: String
         let voteCount: Int
-        
+        let releaseDate: String
+        let video: Bool
+
         enum CodingKeys: String, CodingKey {
+            case genreIDS = "genre_ids"
             case adult
             case backdropPath = "backdrop_path"
-            case genreIds = "genre_ids"
             case id
-            case originalLanguage = "original_language"
             case originalTitle = "original_title"
-            case overview
+            case voteAverage = "vote_average"
             case popularity
             case posterPath = "poster_path"
-            case releaseDate = "release_date"
-            case title
-            case video
-            case voteAverage = "vote_average"
+            case overview, title
+            case originalLanguage = "original_language"
             case voteCount = "vote_count"
+            case releaseDate = "release_date"
+            case video
         }
     }
 }
